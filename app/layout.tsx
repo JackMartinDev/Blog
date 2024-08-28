26eb4c56ea8ce45b651721e2e455380d9976d04b
Jack Martin <jackmartin.dev@gmail.com>
2024-08-28 20:26:01 +0900

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} min-h-screen`}>
+        <Header />
+        <main className="mx-auto max-w-5xl min-h-screen">{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import BlogPost from "./components/blog-post";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-5xl min-h-screen">
-      <div className="flex flex-col gap-4">
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
-      </div>
-    </main>
+    <div className="flex flex-col gap-4">
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+      <BlogPost />
+    </div>
   );
 }
