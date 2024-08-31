c11753e5ad4a9bcbbd4987da95cc4bb69fd819c6
Jack Martin <jackmartin.dev@gmail.com>
2024-08-31 18:58:42 +0900

    Migrate from router to link

--- app/components/blog-post.tsx
+++ app/components/blog-post.tsx
@@ -1,7 +1,6 @@
-"use client";
 import { Post } from "@prisma/client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const formatOptions: Intl.DateTimeFormatOptions = {
   year: "numeric",
@@ -14,30 +13,28 @@ const BlogPost = ({
 }: {
   post: Post;
 }) => {
-  const router = useRouter();
   return (
-    <div
-      className="flex flex-col gap-2 font-light sm:flex-row cursor-pointer border-b last:border-0 p-4 dark:text-slate-50"
-      onClick={() => router.push("/posts/" + slug)}
-    >
-      <div className="flex shrink-0">
-        <Image
-          src={image ?? "/thumbnail.jpg"}
-          alt="Blog post"
-          width={1300}
-          height={866}
-          className="border w-full h-auto sm:w-[320px] sm:h-[180px] object-cover"
-        />
-      </div>
-      <div className="flex flex-col items-start gap-1">
-        <h3 className="text-xl font-semibold">{title}</h3>
-        <p className="">{`Published on ${createdAt.toLocaleDateString("en-US", formatOptions)}`}</p>
-        <p className="line-clamp-5 md:line-clamp-4">{content}</p>
-        <button className="text-sm underline underline-offset-2">
-          Read More
-        </button>
-      </div>
-    </div>
+    <article className="flex flex-col gap-2 font-light sm:flex-row cursor-pointer border-b last:border-0 p-4 dark:text-slate-50">
+      <Link href={`/posts/${slug}`} className="flex shrink-0">
+        <div>
+          <Image
+            src={image ?? "/thumbnail.jpg"}
+            alt="Blog post"
+            width={1300}
+            height={866}
+            className="border w-full h-auto sm:w-[320px] sm:h-[180px] object-cover"
+          />
+        </div>
+      </Link>
+      <Link href={`/posts/${slug}`}>
+        <div className="flex flex-col items-start gap-1">
+          <h3 className="text-xl font-semibold">{title}</h3>
+          <p className="">{`Published on ${createdAt.toLocaleDateString("en-US", formatOptions)}`}</p>
+          <p className="line-clamp-5 md:line-clamp-4">{content}</p>
+          <p className="text-sm underline underline-offset-2">Read More</p>
+        </div>
+      </Link>
+    </article>
   );
 };
 
