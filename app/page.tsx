866ce7d88a4afab8ae21aca005bd3fcdec891957
Jack Martin <jackmartin.dev@gmail.com>
2024-09-01 00:07:57 +0900

    Remove prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import BlogPost from "./components/blog-post";
 import Hero from "./components/hero";
-import prisma from "./lib/db";
+import Posts from "./components/posts";
 
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-  });
   return (
     <>
       <Hero />
-      <div className="flex flex-col gap-4">
-        {posts.map((post) => (
-          <BlogPost key={post.id} post={post} />
-        ))}
-      </div>
+      <Posts />
     </>
   );
 }
