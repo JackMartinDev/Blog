20ce2e50ddc389e66c88e54f0a0969570ea446c1
Jack Martin <jackmartin.dev@gmail.com>
2024-09-04 16:03:38 +0900

    Add JP flag icon

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,13 +2,17 @@ import Link from "next/link";
 import SocialIcon from "./social-icon";
 import ThemeButton from "@/app/components/theme-button";
 import GitHubIcon from "@/app/components/icons/github-icon";
+import JPFlag from "@/app/components/icons/japan-flag";
 
 const Header = () => {
   return (
     <nav className="h-14 border-b sticky top-0 bg-white dark:bg-slate-900 z-10">
       <div className="px-4 flex flex-row justify-between items-center dark:text-slate-50 mx-auto max-w-5xl h-full">
-        <span className="text-3xl font-semibold after:content-['dev\00a0in\00a0🇯🇵'] after:ml-0.5 after:text-slate-400 after:dark:text-slate-400 after:text-base">
+        <span className="text-3xl font-semibold">
           <Link href="/">Jack Martin</Link>
+          <span className="ml-0.5 text-slate-400 dark:text-slate-400 text-base">
+            dev in {<JPFlag />}
+          </span>
         </span>
         <div className="flex flex-row gap-1 items-center">
           <SocialIcon href="https://github.com/JackMartinDev">
---
+++ app/components/icons/japan-flag.tsx
@@ -0,0 +1,16 @@
+const JPFlag = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 900 600"
+    width="24"
+    height="18"
+    className="inline"
+  >
+    <g stroke="black" stroke-width="2">
+      <rect width="900" height="600" fill="#ffffff" />
+      <circle cx="450" cy="300" r="180" fill="#bc002d" />
+    </g>
+  </svg>
+);
+
+export default JPFlag;
